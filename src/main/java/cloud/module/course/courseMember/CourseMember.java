b875835b25755f58c0ec010f4a404095cedf407e
Clavier Zhang <y2595zha@edu.uwaterloo.ca>
2018-07-11 23:33:07 +0800

    fix entity issue

--- src/main/java/cloud/module/course/courseMember/CourseMember.java
+++ src/main/java/cloud/module/course/courseMember/CourseMember.java
@@ -1,4 +1,20 @@
 package cloud.module.course.courseMember;
 
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CourseMember")
+@Data
 public class CourseMember {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator( name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
 }
